11207a9a3a2b12dfcf1b3fc0b27fa0acd8b121ee
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-06-17 23:08:18 -0500

    tarea

---
+++ Dia2/ejercicios.py
@@ -0,0 +1,80 @@
+# EJERCICIO 1
+# ejemplo
+# Para evitar que en cada impresion se ejecute una nueva linea, se puede agregar el parametro end y este indicara como queremos que actue al finalizar la linea
+# for numero in range(5):
+#     print(numero, end="")
+
+
+# EJERCICIO 2
+# Escriba el codigo que le pida al usuario ingresar la altura y el ancho de un rectangulo y
+# que lo dibuje usando *, ejemplo:
+# altura: 5
+# ancho: 4
+# Resultado:
+# ****
+# ****
+# ****
+# ****
+# ****
+
+
+# EJERCICIO 3
+# Escribir el codigo que el usuario le ingrese el grosor de un octagono y que lo dibuje
+# Ejemplo:
+# Grosor: 5
+#       *****
+#      *******
+#     *********
+#    ***********
+#   *************
+#   *************
+#   *************
+#   *************
+#   *************
+#    ***********
+#     *********
+#      *******
+#       *****
+
+
+# EJERCICIO 4
+# De acuerdo a la altura que nosotros ingresemos, nos tiene que dibujar el triangulo
+# invertido
+# Ejemplo
+# Altura: 4
+# ****
+# ***
+# **
+# *
+
+
+# EJERCICIO 5
+# Ingresar un numero entero y ese numero debe de llegar a 1 usando la serie de Collatz
+# si el numero es par, se divide entre dos
+# si el numero es impar, se multiplica por 3 y se suma 1
+# la serie termina cuando el numero es 1
+# Ejemplo 19
+# 19 58 29 88 44 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 12
+
+
+# EJERCICIO 6
+# si el texto de ingreso es:
+texto = "hola alumnos buenas noches ya se viene el break"
+# entonces el texto resultado debera ser:
+resultado_final = ["Hola", "Alumnos", "Buenas", "Noches", "Ya", "Se"]
+# Hacer el codigo para ello
+
+
+# EJERCICIO 7
+# ingresar numeros hasta que ese numero sea adivinado
+numero_adivinar = 10
+# 5 => 'el numero es mayor que ese'
+# 13 => 'el numero es menor que ese'
+# 10 => 'felicidades adivinaste el numero'
+
+
+# EJERCICIO 8
+# dado los siguientes numeros:
+numeros = [1, 2, 5, 9, 12, 15, 17, 19, 21, 39, 45]
+# indicar cuantos de ellos son multiplos de 3 y de 5 , ademas si hay un multiplo de 3 y de 5 no contabilizarlos
+# multiplos de 3: 3 , multiplos de 5: 1
