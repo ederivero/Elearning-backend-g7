ca8d9f783be43b83eb73afbe10bd3478d229b11a
unknown <paholo2054@gmail.com>
2022-07-23 10:30:06 -0500

    update products

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ecommerce',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Productos
 from .serializers import ProductoSerializer
 
@@ -7,3 +8,22 @@ from .serializers import ProductoSerializer
 class ProductoView(generics.ListCreateAPIView):
     queryset = Productos.objects.all()
     serializer_class = ProductoSerializer
+
+    def get(self, request):
+        productos = self.get_serializer(self.get_queryset(), many=True)
+        
+        return Response(data={
+            'success': True,
+            'message': 'La lista de productos es',
+            'content': productos.data
+        }, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        producto = self.get_serializer(data=request.data)
+        if producto.is_valid():
+            producto.save()
+            return Response(data={
+                'success': True,
+                'message': '',
+                'content': producto.data
+            }, status=status.HTTP_201_CREATED)
