71db385f6ef85ec0220daae3106e019f426047c4
unknown <paholo2054@gmail.com>
2022-07-23 12:01:30 -0500

    update

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'drf_yasg',
     'autenticacion',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -10,12 +10,13 @@ class Categorias(models.Model):
     class Meta:
         db_table = 'categorias'
 
-      
+
 class Productos(models.Model):
     productoId = models.AutoField(primary_key=True)
     productoNombre = models.CharField(max_length=200, null=False)
     productoDescripcion = models.CharField(max_length=200)
     productoPrecio = models.DecimalField(max_digits=5, decimal_places=2)
+    productoImagen = models.TextField(default='https://www.google.com')
     estado = models.BooleanField(default=True, null=True)
     categoriaId = models.ForeignKey(Categorias, on_delete=models.CASCADE, related_name='ProductosCategoria')
 
