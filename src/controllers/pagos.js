db89392faa08c959840787908c65870789293dbc
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-08-18 21:16:00 -0500

    avance proceso de pago

--- src/controllers/pagos.js
+++ src/controllers/pagos.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import mercadopago from "mercadopago";
 import { carritoModel } from "../models/carritos.js";
+import { productoModel } from "../models/productos.js";
 
 export const crearUsuarioDePrueba = async (req, res) => {
   try {
@@ -57,10 +58,59 @@ export const crearPago = async (req, res) => {
 
   const carrito = await carritoModel.findOne({ usuarioId: user._id });
 
+  const items = await Promise.all(
+    carrito.detalle.map(async (detalle) => {
+      const producto = await productoModel.findById(detalle.productoId);
+      return {
+        id: producto._id,
+        title: producto.nombre,
+        picture_url: producto.foto,
+        quantity: detalle.cantidad,
+        currency_id: "PEN",
+        unit_price: +producto.precio.toString(),
+      };
+    })
+  );
+
+  const preferencia = await mercadopago.preferences.create({
+    auto_return: "approved", // esto redireccionara automaticamente cuando el pago sea aprobado,
+    back_urls: {
+      success: "http://www.google.com", // la url si el pago es exitoso se hara el redireccionamiento
+      failure: "http://www.tecsup.edu.pe", // si el pago no fue exitosoa se hara el rediccionamiento
+      pending: "http://www.example.com", // si aun no se realizo el pago por que fue seleccionada la opcion de pago contra entrega o PagoEfectivo
+    },
+    payer: {
+      name: user.nombre,
+      surname: user.apellido,
+      email: "test_user_63274575@testuser.com", // user.email NOTA: solamente usar el correo del usuario cuando se use alguna otra token que no sea la de la certificacion mientras tanto usar ese correo definido
+      address: {
+        // completamente opcional
+        street_name: user.direcciones[0]?.calle,
+        street_number: +user.direcciones[0]?.numero,
+        zip_code: "04002",
+      },
+    }, // informacion de la persona que va a pagar
+    items,
+    notification_url:
+      "https://b77a-190-236-76-55.ngrok.io/mercado-pago-notificaciones", // colocamos la url en la cual mercado pago va a enviar la informacion en tiempo real sobre esta preferencia , mercado pago la conoce como IPN (Instant Payment Notification)
+  });
   console.log(carrito);
+
+  console.log(preferencia);
 
   return res.json({
     message: "el link es",
-    result: null,
+    result: preferencia,
   });
+};
+
+export const notificacionesMercadoPago = async (req, res) => {
+  console.log("el body es:");
+  console.log(req.body);
+  console.log("los parametros son:");
+  console.log(req.params);
+  console.log("los query params son:");
+  console.log(req.query);
+
+  return res.status(200).send();
 };
--- src/routes/pagos.js
+++ src/routes/pagos.js
@@ -3,6 +3,7 @@ import {
   crearPreferencia,
   crearUsuarioDePrueba,
   crearPago,
+  notificacionesMercadoPago,
 } from "../controllers/pagos.js";
 
 import { validarToken } from "../utils/validador.js";
@@ -12,3 +13,4 @@ export const pagosRouter = Router();
 pagosRouter.route("/crearUsuarioDePrueba").post(crearUsuarioDePrueba);
 pagosRouter.route("/crearPreferencia").post(crearPreferencia);
 pagosRouter.post("/crear-pago", validarToken, crearPago);
+pagosRouter.post("/mercado-pago-notificaciones", notificacionesMercadoPago);
